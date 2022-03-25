1910893163b09d924a69530ce0f08a766a11748f
Clea <f.clea33@gmail.com>
2022-03-25 13:39:51 +0100

    a

---
+++ base/footer.php
@@ -0,0 +1,8 @@
+<?php
+echo '    <footer class="text-center">Contact (insta,fb,twt,linkedin,mail)</footer>
+<script src="../assets/vendors/jquery-3.6.0.js"></script>
+<script src="../assets/vendors/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>'
+?>
