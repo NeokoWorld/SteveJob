2e904e44690f2c290e4ea75fac76151bafcc18fd
Clea <f.clea33@gmail.com>
2022-03-28 09:52:49 +0200

    Creation

--- creation/creation.php
+++ creation/creation.php
@@ -9,10 +9,20 @@ echo '<main>
             <fieldset>
                 <legend>Création d\'un compte</legend>
                 <div class="row">
-                    <div class="col-3"><input type="radio" name="compte" value="Pilote" required /></div>
-                    <div class="col-3"><label for="Pilote">Pilote</label></div>
-                    <div class="col-3"><input type="radio" name="compte" value="Eleve" required /></div>
-                    <div class="col-3"><label for="Eleve">Eleve</label></div>
+                    <div class="col-4"></div><div class="col-1"><input type="radio" name="compte" value="Pilote" required /></div>
+                    <div class="col-1"><label for="Pilote">Pilote</label></div>
+                    <div class="col-1"><input type="radio" name="compte" value="Eleve" required /></div>
+                    <div class="col-1"><label for="Eleve">Eleve</label></div><div class="col-4"></div>
+                    <div class="col-6"><label for="Nom">Nom</label></div>
+                    <div class="col-6"><input type="text" name="Nom" placeholder="Saisissez votre nom" required/></div>
+                    <div class="col-6"><label for="Prenom">Prenom</label></div>
+                    <div class="col-6"><input type="text" name="Prenom" placeholder="Saisissez votre prénom" required/></div>
+                    <div class="col-6"><label for="Centre">Centre</label></div>
+                    <div class="col-6"><input type="text" name="Centre" placeholder="Saisissez votre centre de formation" required/></div>
+                    <div class="col-6"><label for="Promotion">Promotion</label></div>
+                    <div class="col-6"><input type="text" name="Promotion" placeholder="Indiquez votre promotion" required/></div>
+                    <div class="col-3"></div><div class="col-3"><input type="submit" value="Envoyer" id="envoyer" /></div>
+                    <div class="col-3"><input type="reset" value="Annuler" /></div><div class="col-3"></div>
                 </div>
             </fieldset>
         </form>
