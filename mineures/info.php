f6c436de5466e5a121117b9d5b14e1805f0fadaf
Thomas Ballini <thomasballini34@gmail.com>
2022-03-28 10:40:19 +0200

    sdfg

--- mineures/info.php
+++ mineures/info.php
@@ -6,10 +6,32 @@ echo '<main>
 <div class="container">
     <div class="row">
         <article class="col-sm-1"></article>
-        <article class="col-sm-3"></article>
-        <article class="col-sm-8"></article>
+        <article class="col-sm-11">';
+        require '../PHP/Class.php';
+        $users = new Offre();
+foreach($users->getOffre() as $user)
+{   
+    $lien = "";
+    /*echo '<pre>';
+    echo $user['id_offre'] , " " ;
+    echo $user['competences'] , " ";
+    echo $user['localite'] , " ";
+    echo $user['entreprise'] , " ";
+    echo $user['duree'] , " ";
+    echo $user['remuneration'] , " ";
+    echo $user['date_offre'] , " ";
+    echo $user['id_fiche'] , " ";
+    echo '</pre>';*/
+
+    $lien =  $user['id_offre']." ".$user['competences']." ".$user['localite']." ".$user['entreprise']." ".$user['duree']." ".$user['remuneration']." ".$user['date_offre']." ".$user['id_fiche']." ";
+
+    echo "<a href = './offre.php?idOffre=".$user['id_offre']."'>".$lien."</a>";
+    echo '<br> <br>';
+}
+$users->getOffre();
+        echo '</article>
     </div>
 </div>
 </main>';
-    include '../Base/footer.php';
-    ?>
+include '../Base/footer.php';
+?>
