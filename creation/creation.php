89fdc1f303168006aaeb5a3c869554d067f6c124
Clea <f.clea33@gmail.com>
2022-03-28 10:10:35 +0200

    oof

--- creation/creation.php
+++ creation/creation.php
@@ -5,9 +5,9 @@ include '../Base/header.php';
 echo '<main>
 <div class="container">
     <div class="row">
-        <form action="" method="post" class="text-center formu">
+        <form action="" method="post" class="text-center">
             <fieldset>
-                <legend>Création d\'un compte</legend>
+                <legend>Création d\'un profil</legend>
                 <div class="row">
                     <div class="col-4"></div><div class="col-1"><input type="radio" name="compte" value="Pilote" required /></div>
                     <div class="col-1"><label for="Pilote">Pilote</label></div>
