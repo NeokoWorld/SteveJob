737a77038948bb66cddc277891a642040ea422bd
Clea <f.clea33@gmail.com>
2022-03-28 10:58:38 +0200

    Fait gaffe
    Un <div class=\"bdd\"> peut avoir sauté dans le echo de $lien

--- mineures/info.php
+++ mineures/info.php
@@ -25,8 +25,7 @@ foreach($users->getOffre() as $user)
 
     $lien =  $user['id_offre']." ".$user['competences']." ".$user['localite']." ".$user['entreprise']." ".$user['duree']." ".$user['remuneration']." ".$user['date_offre']." ".$user['id_fiche']." ";
 
-    echo "<a href = './offre.php?idOffre=".$user['id_offre']."'>".$lien."</a>";
-    echo '<br> <br>';
+    echo "<div class=\"bdd\"><a href = './offre.php?idOffre=".$user['id_offre']."'>".$lien."</a></div>";
 }
 $users->getOffre();
         echo '</article>
