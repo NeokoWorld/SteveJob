d379902a78461404581b617b93e14edda692678f
Thomas Ballini <thomasballini34@gmail.com>
2022-03-29 14:43:26 +0200

    belle lsite

--- listes/l-eleve.php
+++ listes/l-eleve.php
@@ -12,53 +12,66 @@ echo '<main>
                 </a>
                 <div class="text-center col-11">
                     <article class="prof">Liste des élèves';
-                    require '../PHP/Class.php';
-                    $users = new Eleve();
-                    if (isset($_GET['page']) && !empty($_GET['page'])){
-                        $page=$_GET['page']-1;
-                    }else{
-                     $page=0;
-                    }
-                    foreach($users->getEleve($page*10) as $user)
-                    {   
-                        echo '<div class="bdd"> ';
-                        echo $user['id_eleve'] , " " ;
-                        echo $user['nom'] , " ";
-                        echo $user['prenom'] , " ";
-                        echo $user['centre'] , " ";
-                        echo $user['Promotion'] , " ";
-                        echo $user['email'] , " ";
-                        echo $user['id_user'] , " ";
-                        echo '</div>';
-                    }
-                    $users->getEleve();
-                    $toutesLignes=(int)$users->compterEleve();
-                    $totoalPages = ceil($toutesLignes/10);
-                    if(isset($_GET['page']) && !empty($_GET['page'])){
-                        $currentPage = (int) strip_tags($_GET['page'])-1;
-                    }else{
-                    $currentPage = 0;
+require '../PHP/Class.php';
+$users = new Eleve();
+if (isset($_GET['page']) && !empty($_GET['page'])) {
+    $page = $_GET['page'] - 1;
+} else {
+    $page = 0;
+}
+foreach ($users->getEleve($page * 10) as $user) {
+    echo '<div class="bdd"> ';
+    echo $user['id_eleve'], " ";
+    echo $user['nom'], " ";
+    echo $user['prenom'], " ";
+    echo $user['centre'], " ";
+    echo $user['Promotion'], " ";
+    echo $user['email'], " ";
+    echo $user['id_user'], " ";
+    echo '</div>';
+}
+$users->getEleve();
+$toutesLignes = (int)$users->compterEleve();
+$totoalPages = ceil($toutesLignes / 10);
+if (isset($_GET['page']) && !empty($_GET['page'])) {
+    $currentPage = (int) strip_tags($_GET['page']) - 1;
+} else {
+    $currentPage = 0;
 };
-                    
+
 ?>
 
 <nav>
     <ul class="pagination justify-content-center">
-        <li class="page-item <?php if($page<=0){echo 'disabled';} ?>">
-            <a class="page-link" href="<?php if($page<0){echo '#';} else{echo "?page=".($currentPage-1);}?>">Precedent</a>
+        <li class="page-item <?php if ($page <= 0) {
+                                    echo 'disabled';
+                                } ?>">
+            <a class="page-link" href="<?php if ($page < 0) {
+                                            echo '#';
+                                        } else {
+                                            echo "?page=" . ($currentPage - 1);
+                                        } ?>">Precedent</a>
         </li>
-        <?php for($i=1;$i <=$totoalPages; $i++): ?>
-        <li class="page-item <?php if(($page+1)==$i){echo 'active';} ?>">
-            <a class="page-link" href="?page=<?=$i;?>"><?=$i;?></a>
-        </li>
+        <?php for ($i = 1; $i <= $totoalPages; $i++) : ?>
+            <li class="page-item <?php if (($page + 1) == $i) {
+                                        echo 'active';
+                                    } ?>">
+                <a class="page-link" href="?page=<?= $i; ?>"><?= $i; ?></a>
+            </li>
         <?php endfor; ?>
-        <li class="page-item <?php if(($page+1)>=$totoalPages){echo 'disabled';} ?>">
-            <a class="page-link" href="<?php if($page>=$totoalPages){echo '#';} else{echo "?page=".($currentPage+2);}?>">Suivant</a>
+        <li class="page-item <?php if (($page + 1) >= $totoalPages) {
+                                    echo 'disabled';
+                                } ?>">
+            <a class="page-link" href="<?php if ($page >= $totoalPages) {
+                                            echo '#';
+                                        } else {
+                                            echo "?page=" . ($currentPage + 2);
+                                        } ?>">Suivant</a>
         </li>
     </ul>
 </nav>
 
-                   <?php echo '</article>
+<?php echo '</article>
                 </div>
             </div>
         </div>
