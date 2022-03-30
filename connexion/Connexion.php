ca686042672cc133aae00c062ee21e388210214c
Clea <f.clea33@gmail.com>
2022-03-30 09:20:22 +0200

    debut co

--- connexion/Connexion.php
+++ connexion/Connexion.php
@@ -1,18 +1,19 @@
 <?php
 include '../Base/head.php';
 echo '<link rel="stylesheet" href="connexion.css">';
-include '../Base/header.php';
-echo '<main>
+include '../Base/header.php';?>
+
+<main>
         <div>
             <form method="post" action="">
                 <fieldset>
                     <legend>Connexion</legend>
                     <div class="container">
                         <div class="row"><label class="col-sm-6">Email</label>
-                            <input type="email" class="col-sm-6" placeholder="Entrez votre Email" id="Email" required />
+                            <input type="text" class="col-sm-6" placeholder="Entrez votre Login" id="login" name="login" required />
                         </div>
                         <div class="row"><label class="col-sm-6">Mot de passe</label>
-                            <input type="password" class="col-sm-6" placeholder="Entrez votre Mot de Passe" id="mdp"
+                            <input type="password" class="col-sm-6" placeholder="Entrez votre Mot de Passe" id="mdp" name="mdp"
                                 minlength="8" required />
                         </div>
                     </div>
@@ -22,5 +23,6 @@ echo '<main>
             </form>
             <a href="../connexion/mdp_oub.php">Mot de passe oublié ?</a>
         </div>
-    </main>';
-include '../Base/footer.php';
+    </main>
+
+<?php include '../Base/footer.php';?>
