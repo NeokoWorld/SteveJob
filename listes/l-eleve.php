3954b62ac26d586c37eb5202b65c86e9ee14be43
Clea <f.clea33@gmail.com>
2022-03-30 14:32:47 +0200

    Anti retour

--- listes/l-eleve.php
+++ listes/l-eleve.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+if(@$_SESSION['auth']==true){
 include '../Base/head.php';
 echo '<link rel="stylesheet" href="liste.css">';
 include '../Base/header.php';
@@ -103,13 +104,16 @@ if (isset($_GET['page']) && !empty($_GET['page'])) {
                              break;
                     }
                 }
-                ?>
-                <div>
+                echo '<div>
                     <a class="deco" href="../deco/deconnexion.php">Deconnexion</a>
                 </div>
             </div>
         </div>
-    </main>
+    </main>';
 
-<?php echo include '../Base/footer.php';
+echo include '../Base/footer.php';
+}else{
+    header("Location:../connexion/connexion.php");
+    exit;
+}
 ?>
