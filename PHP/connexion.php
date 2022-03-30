27dcf5bb1db8912906f58d6999a198f4243f3c8d
Thomas Ballini <thomasballini34@gmail.com>
2022-03-30 09:48:21 +0200

    con 2

--- PHP/connexion.php
+++ PHP/connexion.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'Class.php';
 
 $co= new LoginRepository();
@@ -6,9 +7,10 @@ $verif=$co->login($_POST['login'],$_POST['mdp']);
 print_r($verif);
 
 if(count($verif)>0){
-     
-}else{
-    echo "L'identifiant ou le mot de passe est incorrecte"
+     $_SESSION['auth']=true;
+}
+else{
+    echo "L'identifiant ou le mot de passe est incorrecte";
 }
 
 ?>
