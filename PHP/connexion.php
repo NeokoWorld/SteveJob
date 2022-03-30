bc1d5a2886dcd3e6fda74ed5c9756a641d387ca7
Thomas Ballini <thomasballini34@gmail.com>
2022-03-30 10:36:38 +0200

    marche

--- PHP/connexion.php
+++ PHP/connexion.php
@@ -4,12 +4,14 @@ include 'Class.php';
 
 $co= new LoginRepository();
 $verif=$co->login($_POST['login'],$_POST['mdp']);
-print_r($verif);
+//print_r($verif);
 
-if(count($verif)>0){
-     $_SESSION['auth']=true;
-     $_SESSION['user']['ID_Role']=$verif[0]['ID_Role'];
-     header("Location : ../mainpage/mainPage.php");
+if(count($verif)>0)
+{
+    $_SESSION['auth']=true;
+    $_SESSION['user']['ID_Role']=$verif[0]['ID_Role'];
+    header("Location:../mainpage/mainPage.php");
+    exit;
 }
 else{
     echo "L'identifiant ou le mot de passe est incorrecte";
