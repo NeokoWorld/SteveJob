4756554df8d8eb508aebc23469cc049770eb1cc3
Thomas Ballini <thomasballini34@gmail.com>
2022-04-01 11:52:28 +0200

    search amélioré

--- search/search.php
+++ search/search.php
@@ -15,7 +15,10 @@ echo '<main>
 
 require'../PHP/Class.php';
 
-$names = new Recherche();
+if(@$_SESSION['auth']==true){
+    switch ($_SESSION['user']['ID_Role']){
+        case 1 :
+            $names = new Recherche();
 foreach ($names->getElevebyName($_GET['Recherche']) as $name) 
 {
     $lien = "";
@@ -59,10 +62,159 @@ foreach ($competences->getOffrebyComp($_GET['Recherche']) as $competence)
     $lien =  $competence['id_offre']." "."|"." ".$competence['localite']." "."|"." ".$competence['entreprise']." "."|"." ".$competence['competences']." "."|"." ".$competence['duree']." ". 'semaines'." "."|"." ".$competence['remuneration']." ". '€' ." "."|"." ".date_format($date, 'd-m-Y')." "."|"." ".$competence['id_fiche']." ";
     echo "<div class=\"offre\"><h5><strong>Offre de stage</strong></h5><a class=\"joie\" href = '../mineures/offre.php?idOffre=".$competence['id_offre']."'><b>".$lien."</b></a></div>";
 }
-
-
-
 $competences->getOffrebyComp($_GET['Recherche']);
+            break;
+        
+        
+        
+        case 2 :
+            $names = new Recherche();
+foreach ($names->getElevebyName($_GET['Recherche']) as $name) 
+{
+    $lien = "";
+    $lien =  $name['nom']." "."|"." ".$name['prenom']." "."|"." ".$name['centre']." "."|"." ".$name['email']." "."|"." ".$name['id_user']." ";
+    echo "<div class=\"etudiant\"><h5><strong>Etudiant</strong></h5><a class=\"joie\" href = '../profil/etudiant.php?idUser=".$name['id_user']."'><b>".$lien."</b></a></div>";
+}
+$names->getElevebyName($_GET['Recherche']);
+
+$names = new Recherche();
+foreach ($names->getDeleguebyName($_GET['Recherche']) as $name) 
+{
+    $lien = "";
+    $lien =  $name['nom']." "."|"." ".$name['prenom']." "."|"." ".$name['centre']." "."|"." ".$name['email']." "."|"." ".$name['id_user']." ";
+    echo "<div class=\"delegue\"><h5><strong>Delegue</strong></h5><a class=\"joie\" href = '../profil/jean.php?idUser=".$name['id_user']."'><b>".$lien."</b></a></div>";
+}
+$names->getDeleguebyName($_GET['Recherche']);
+
+$names = new Recherche();
+foreach ($names->getEntreprisebyName($_GET['Recherche']) as $name) 
+{
+    $lien = "";
+    $lien =  $name['Nom']." "."|"." ".$name['Secteur_activite']." "."|"." ".$name['Localite']." "."|"." ".$name['Nb_stagiaire_cesi']." "." stagiaires "."|"." ".$name['evaluation_stagiaire']."/5 "."|"." ".$name['confiance_pilote']."/5 "." ";
+    echo "<div class=\"entreprise\"><h5><strong>Entreprise</strong></h5><a class=\"joie\" href = '../profil/entreprise.php?idFiche=".$name['id_fiche']."'><b>".$lien."</b></a></div>";
+}
+$names->getEntreprisebyName($_GET['Recherche']);
+
+$competences = new Recherche();
+foreach ($competences->getOffrebyComp($_GET['Recherche']) as $competence) 
+{
+    $date = new DateTime($competence['date_offre']);
+    $lien = "";
+    $lien =  $competence['id_offre']." "."|"." ".$competence['localite']." "."|"." ".$competence['entreprise']." "."|"." ".$competence['competences']." "."|"." ".$competence['duree']." ". 'semaines'." "."|"." ".$competence['remuneration']." ". '€' ." "."|"." ".date_format($date, 'd-m-Y')." "."|"." ".$competence['id_fiche']." ";
+    echo "<div class=\"offre\"><h5><strong>Offre de stage</strong></h5><a class=\"joie\" href = '../mineures/offre.php?idOffre=".$competence['id_offre']."'><b>".$lien."</b></a></div>";
+}
+$competences->getOffrebyComp($_GET['Recherche']);
+            break;
+        
+        
+        
+        case 3 :
+            $names = new Recherche();
+foreach ($names->getElevebyName($_GET['Recherche']) as $name) 
+{
+    $lien = "";
+    $lien =  $name['nom']." "."|"." ".$name['prenom']." "."|"." ".$name['centre']." "."|"." ".$name['email']." "."|"." ".$name['id_user']." ";
+    echo "<div class=\"etudiant\"><h5><strong>Etudiant</strong></h5><a class=\"joie\" href = '../profil/etudiant.php?idUser=".$name['id_user']."'><b>".$lien."</b></a></div>";
+}
+$names->getElevebyName($_GET['Recherche']);
+
+$names = new Recherche();
+
+$names = new Recherche();
+foreach ($names->getEntreprisebyName($_GET['Recherche']) as $name) 
+{
+    $lien = "";
+    $lien =  $name['Nom']." "."|"." ".$name['Secteur_activite']." "."|"." ".$name['Localite']." "."|"." ".$name['Nb_stagiaire_cesi']." "." stagiaires "."|"." ".$name['evaluation_stagiaire']."/5 "."|"." ".$name['confiance_pilote']."/5 "." ";
+    echo "<div class=\"entreprise\"><h5><strong>Entreprise</strong></h5><a class=\"joie\" href = '../profil/entreprise.php?idFiche=".$name['id_fiche']."'><b>".$lien."</b></a></div>";
+}
+$names->getEntreprisebyName($_GET['Recherche']);
+
+$competences = new Recherche();
+foreach ($competences->getOffrebyComp($_GET['Recherche']) as $competence) 
+{
+    $date = new DateTime($competence['date_offre']);
+    $lien = "";
+    $lien =  $competence['id_offre']." "."|"." ".$competence['localite']." "."|"." ".$competence['entreprise']." "."|"." ".$competence['competences']." "."|"." ".$competence['duree']." ". 'semaines'." "."|"." ".$competence['remuneration']." ". '€' ." "."|"." ".date_format($date, 'd-m-Y')." "."|"." ".$competence['id_fiche']." ";
+    echo "<div class=\"offre\"><h5><strong>Offre de stage</strong></h5><a class=\"joie\" href = '../mineures/offre.php?idOffre=".$competence['id_offre']."'><b>".$lien."</b></a></div>";
+}
+$competences->getOffrebyComp($_GET['Recherche']);
+            break;
+        
+        
+        
+        
+        case 4 :
+
+$names = new Recherche();
+foreach ($names->getEntreprisebyName($_GET['Recherche']) as $name) 
+{
+    $lien = "";
+    $lien =  $name['Nom']." "."|"." ".$name['Secteur_activite']." "."|"." ".$name['Localite']." "."|"." ".$name['Nb_stagiaire_cesi']." "." stagiaires "."|"." ".$name['evaluation_stagiaire']."/5 "."|"." ".$name['confiance_pilote']."/5 "." ";
+    echo "<div class=\"entreprise\"><h5><strong>Entreprise</strong></h5><a class=\"joie\" href = '../profil/entreprise.php?idFiche=".$name['id_fiche']."'><b>".$lien."</b></a></div>";
+}
+$names->getEntreprisebyName($_GET['Recherche']);
+
+$competences = new Recherche();
+foreach ($competences->getOffrebyComp($_GET['Recherche']) as $competence) 
+{
+    $date = new DateTime($competence['date_offre']);
+    $lien = "";
+    $lien =  $competence['id_offre']." "."|"." ".$competence['localite']." "."|"." ".$competence['entreprise']." "."|"." ".$competence['competences']." "."|"." ".$competence['duree']." ". 'semaines'." "."|"." ".$competence['remuneration']." ". '€' ." "."|"." ".date_format($date, 'd-m-Y')." "."|"." ".$competence['id_fiche']." ";
+    echo "<div class=\"offre\"><h5><strong>Offre de stage</strong></h5><a class=\"joie\" href = '../mineures/offre.php?idOffre=".$competence['id_offre']."'><b>".$lien."</b></a></div>";
+}
+$competences->getOffrebyComp($_GET['Recherche']);
+            break;
+    }
+
+}else{
+    echo '<a class="navbar-brand pad" href="../mainpage/mainPage.php"></a>';
+}
+
+// $names = new Recherche();
+// foreach ($names->getElevebyName($_GET['Recherche']) as $name) 
+// {
+//     $lien = "";
+//     $lien =  $name['nom']." "."|"." ".$name['prenom']." "."|"." ".$name['centre']." "."|"." ".$name['email']." "."|"." ".$name['id_user']." ";
+//     echo "<div class=\"etudiant\"><h5><strong>Etudiant</strong></h5><a class=\"joie\" href = '../profil/etudiant.php?idUser=".$name['id_user']."'><b>".$lien."</b></a></div>";
+// }
+// $names->getElevebyName($_GET['Recherche']);
+
+// $names = new Recherche();
+// foreach ($names->getDeleguebyName($_GET['Recherche']) as $name) 
+// {
+//     $lien = "";
+//     $lien =  $name['nom']." "."|"." ".$name['prenom']." "."|"." ".$name['centre']." "."|"." ".$name['email']." "."|"." ".$name['id_user']." ";
+//     echo "<div class=\"delegue\"><h5><strong>Delegue</strong></h5><a class=\"joie\" href = '../profil/jean.php?idUser=".$name['id_user']."'><b>".$lien."</b></a></div>";
+// }
+// $names->getDeleguebyName($_GET['Recherche']);
+
+// $names = new Recherche();
+// foreach ($names->getPilotebyName($_GET['Recherche']) as $name) 
+// {
+//     $lien = "";
+//     $lien =  $name['nom']." "."|"." ".$name['prenom']." "."|"." ".$name['centre']." "."|"." ".$name['email']." "."|"." ".$name['id_user']." ";
+//     echo "<div class=\"pilote\"><h5><strong>Pilote</strong></h5><a class=\"joie\" href = '../profil/avion.php?idPilote=".$name['id_user']."'><b>".$lien."</b></a></div>";
+// }
+// $names->getPilotebyName($_GET['Recherche']);
+
+// $names = new Recherche();
+// foreach ($names->getEntreprisebyName($_GET['Recherche']) as $name) 
+// {
+//     $lien = "";
+//     $lien =  $name['Nom']." "."|"." ".$name['Secteur_activite']." "."|"." ".$name['Localite']." "."|"." ".$name['Nb_stagiaire_cesi']." "." stagiaires "."|"." ".$name['evaluation_stagiaire']."/5 "."|"." ".$name['confiance_pilote']."/5 "." ";
+//     echo "<div class=\"entreprise\"><h5><strong>Entreprise</strong></h5><a class=\"joie\" href = '../profil/entreprise.php?idFiche=".$name['id_fiche']."'><b>".$lien."</b></a></div>";
+// }
+// $names->getEntreprisebyName($_GET['Recherche']);
+
+// $competences = new Recherche();
+// foreach ($competences->getOffrebyComp($_GET['Recherche']) as $competence) 
+// {
+//     $date = new DateTime($competence['date_offre']);
+//     $lien = "";
+//     $lien =  $competence['id_offre']." "."|"." ".$competence['localite']." "."|"." ".$competence['entreprise']." "."|"." ".$competence['competences']." "."|"." ".$competence['duree']." ". 'semaines'." "."|"." ".$competence['remuneration']." ". '€' ." "."|"." ".date_format($date, 'd-m-Y')." "."|"." ".$competence['id_fiche']." ";
+//     echo "<div class=\"offre\"><h5><strong>Offre de stage</strong></h5><a class=\"joie\" href = '../mineures/offre.php?idOffre=".$competence['id_offre']."'><b>".$lien."</b></a></div>";
+// }
+// $competences->getOffrebyComp($_GET['Recherche']);
 
 echo '</article>
                 </div>
@@ -86,4 +238,3 @@ echo '</article>
     </main>';
 
 include '../Base/footer.php';
-?>
